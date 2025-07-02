a005d35799006463b064f48ade8bdf9a0e649f95
Abdullah Al Mahfuz <abdullah@appnap.io>
2025-07-02 16:05:49 +0600

    Haptic reflect on touches

--- Custom-Vibrator/ViewController.swift
+++ Custom-Vibrator/ViewController.swift
@@ -6,8 +6,13 @@
 //
 
 import UIKit
+import CoreHaptics
 
 class ViewController: UIViewController {
+    
+    var hapticEngine: CHHapticEngine?
+    var hapticPlayer: CHHapticAdvancedPatternPlayer?
+
     
     private lazy var touchLabel: UILabel = {
         let label = UILabel()
@@ -22,8 +27,44 @@ class ViewController: UIViewController {
         print("hellow")
         view.backgroundColor = UIColor.purple
         setupUI()
+        prepareHaptics()
     }
 
+    private func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        do {
+            hapticEngine = try CHHapticEngine()
+            try hapticEngine?.start()
+        } catch {
+            print("could not access haptic")
+        }
+        
+    }
+    
+    private func startHaptic() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0.5)
+        
+        let event = CHHapticEvent(
+            eventType: .hapticContinuous,
+            parameters: [intensity, sharpness],
+            relativeTime: 0,
+            duration: 10)
+        
+        do {
+            let pattern = try CHHapticPattern(events: [event], parameters: [])
+            hapticPlayer = try hapticEngine?.makeAdvancedPlayer(with: pattern)
+            try hapticPlayer?.start(atTime: 0)
+        } catch {
+            print("could not start haptic engine!")
+        }
+    }
+
+    private func stopHaptic() {
+        try? hapticPlayer?.stop(atTime: 0)
+    }
     
     private func setupUI() {
         view.addSubview(touchLabel)
@@ -35,12 +76,14 @@ class ViewController: UIViewController {
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         print("**touchesBegan")
-        touchLabel.text = "touchesBegan"
+        touchLabel.text = "Haptic Began"
+        startHaptic()
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         print("**touchEnded")
-        touchLabel.text = "touchEnded"
+        touchLabel.text = "Haptic Ended"
+        stopHaptic()
     }
     
 }
