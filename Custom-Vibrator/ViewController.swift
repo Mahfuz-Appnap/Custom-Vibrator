86d37c544f7395321b14be18b21bd7f5824c566d
Abdullah Al Mahfuz <abdullah@appnap.io>
2025-07-03 18:06:19 +0600

    progress bar added in timelineview

--- Custom-Vibrator/ViewController.swift
+++ Custom-Vibrator/ViewController.swift
@@ -12,13 +12,30 @@ class ViewController: UIViewController {
     
     var hapticEngine: CHHapticEngine?
     var hapticPlayer: CHHapticAdvancedPatternPlayer?
-
+    
+    let totalDuration: TimeInterval = 10.0
+    var timelineStart: TimeInterval?
+    var displayLink: CADisplayLink?
+    
     
     private lazy var touchLabel: UILabel = {
         let label = UILabel()
         label.textColor = .white
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
+    }()
+    
+    private lazy var timelineView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    private lazy var playHeaderView: UIView = {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 2, height: 20))
+        view.backgroundColor = .red
+        return view
     }()
 
     override func viewDidLoad() {
@@ -68,15 +85,50 @@ class ViewController: UIViewController {
     
     private func setupUI() {
         view.addSubview(touchLabel)
+        view.addSubview(timelineView)
         NSLayoutConstraint.activate([
             touchLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            touchLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -30)
+            touchLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -15),
+            
+            timelineView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            timelineView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+            timelineView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40),
+            timelineView.heightAnchor.constraint(equalToConstant: 20)
         ])
+        
+        timelineView.addSubview(playHeaderView)
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.startTimeLine()
+        }
+    }
+    
+    private func startTimeLine() {
+        timelineStart = CACurrentMediaTime()
+        displayLink = CADisplayLink(target: self, selector: #selector(updatePlayhead))
+        displayLink?.add(to: .main, forMode: .default)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + totalDuration) {
+            self.displayLink?.invalidate()
+            self.playHeaderView.removeFromSuperview()
+        }
     }
 
+    @objc func updatePlayhead() {
+        guard let start = timelineStart else { return }
+        let elapsedTime = CACurrentMediaTime() - start
+        let progress = min(1, (elapsedTime / totalDuration))
+        
+        let barWidth = timelineView.bounds.width
+        let x = progress * barWidth
+        playHeaderView.frame.origin.x = x
+    }
+    
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         print("**touchesBegan")
         touchLabel.text = "Haptic Began"
+        guard timelineStart != nil else { return }
         startHaptic()
     }
     
